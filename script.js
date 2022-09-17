136676e06938791fc40e5081aa476ed9c0f9d3a6
Darlan Carvalho <darlan171201@gmail.com>
2022-09-17 15:16:20 -0300

    HTML e CSS feitos, estarei indo fazer a parte responsiva do site, e após isso será feito todo a parte do Javascript

---
+++ script.js
@@ -0,0 +1,9 @@
+const campoDeFormulario = document.querySelectorAll('.formulario');
+
+const numeroNovo = document.getElementById('numero-novo');
+
+const nomeNovo = document.getElementById('nome-novo');
+
+const validadeNova = document.getElementById('validade-nova');
+
+const submit = document.getElementById('submit');
