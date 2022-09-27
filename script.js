78689a84ff628b21755460c4304bb06753810510
Darlan Carvalho <darlan171201@gmail.com>
2022-09-26 21:03:34 -0300

    Adicionada uma parte do Javascript.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const campoDeFormulario = document.querySelectorAll('.formulario');
+const campoDeFormulario = document.querySelectorAll('.form');
 
 const numeroNovo = document.getElementById('numero-novo');
 
@@ -7,3 +7,15 @@ const nomeNovo = document.getElementById('nome-novo');
 const validadeNova = document.getElementById('validade-nova');
 
 const submit = document.getElementById('submit');
+
+
+submit.addEventListener('click', function(){
+    campoDeFormulario.forEach(function(input){
+        if(input.value == ''){
+            input.classList.add('invalido')
+            input.classList.remove('valido')
+        }else{
+            input.classList.add('valido')
+        }
+    })
+})
